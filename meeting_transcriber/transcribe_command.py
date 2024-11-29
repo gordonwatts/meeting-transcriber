d188eef0317b4934cceb467e8e7e11ebce7cda9e
Gordon Watts <gwatts@uw.edu>
2024-11-29 04:48:24 -0800

    Remove testing arguments

--- meeting_transcriber/transcribe_command.py
+++ meeting_transcriber/transcribe_command.py
@@ -14,7 +14,7 @@ def transcribe(audio_file: Path, output: Path = Path("./transcription.txt")):
     """
     Transcribe an audio file to text and write it to the output.
     """
-    transcription = transcribe_to_text_file(audio_file, chunk_length=1, num_chunks=1)
+    transcription = transcribe_to_text_file(audio_file)
     cleaned_transcription = clean_and_format_transcription(transcription)
     output.write_text(cleaned_transcription, encoding="utf-8")
 
